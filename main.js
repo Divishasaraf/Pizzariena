c5e1778ac4d2d479f6daef41076a633b3488e0ab
Divi <82699192+Divishasaraf@users.noreply.github.com>
2021-09-04 13:40:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+menu_list_array = ["Veg Margherita Pizza",
+"Pizza Marinara",
+"Pizza Margherita",
+"Pizza Margherita extra",
+"Chicago Pizza",
+"New York-Style Pizza",
+"Sicilian Pizza",
+"Greek Pizza",
+"California Pizza",
+"Detroit Pizza",
+" St. Louis Pizza",
+"Thin Crust Pizza",
+"Cheese Burst Pizza",
+"Pepperoni Pizza",
+"Margherita Pizza",
+"BBQ Chicken Pizza",
+"Hawaiian Pizza",
+"Meat-Lover's Pizza"
+];
+
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById(" display_name   ").innerHTML = htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+        menu_list_array.sort()
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata = htmldata + imgtags + menu_list_array[i] + '<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
